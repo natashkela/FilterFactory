4b5f5b9be7eaf501505033e683eef915eed6220d
Nata Vacheishvili <nvacheishvili@wpi.edu>
2020-01-12 21:49:32 -0500

    no longer needed

--- js/main.js
+++ js/main.js
@@ -593,22 +593,6 @@ if ($("[data-aos]").length) {
 		}
 	});
 
-	// $('#fine-uploader-gallery').fineUploader({
-	// 	template: 'qq-template-gallery',
-	// 	request: {
-	// 		endpoint: '/server/uploads'
-	// 	},
-	// 	thumbnails: {
-	// 		placeholders: {
-	// 			waitingPath: '/source/placeholders/waiting-generic.png',
-	// 			notAvailablePath: '/source/placeholders/not_available-generic.png'
-	// 		}
-	// 	},
-	// 	validation: {
-	// 		allowedExtensions: ['jpeg', 'jpg', 'gif', 'png']
-	// 	}
-	// });
-
 	//convert Price
 
 	$("#price").keyup(function () {
