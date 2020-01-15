43240e4462ac53745032a7229b0e0a5be6595917
Nata Vacheishvili <nvacheishvili@wpi.edu>
2020-01-14 23:07:59 -0500

    123

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -2,7 +2,7 @@
 
 $dbServername = "filterfactory.app";
 $dbUsername = "root";
-$dbPassword = "";
+$dbPassword = "pass";
 $dbName = "filterfa_main_db";
 
-$conn = mysqli_connect($dbServername, $dbUsername,$dbName, $dbPassword);
+$conn = mysqli_connect($dbServername, $dbUsername,$dbPassword,d$dbName);
