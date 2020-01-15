a2245e4383cb30458f238f15a9774937e60e0be9
Nata Vacheishvili <nvacheishvili@wpi.edu>
2020-01-15 08:23:05 -0500

    Update signup.inc.php

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -2,9 +2,10 @@
   if (!empty($_POST)) {
     require 'dbh.inc.php';
 
-//    print_r("<pre>");
-//    print_r($_POST);
-//    exit(1);
+    if (!$conn) {
+        printf("Connect failed: %s\n", mysqli_connect_error());
+        exit();
+    }
     $first = $_POST['first'];
     $last = $_POST['last'];
     $email = $_POST['email-id'];
@@ -22,42 +23,26 @@
     }
     else {
       $sql ="SELECT email from users WHERE email=?";
-      $stmt = mysqli_stmt_init($conn);
-      if (!mysqli_stmt_prepare($stmt, $sql)) {
-        header("Location: ../register.php?error=sqlerror");
+      $sql->bind_param("s", $email);
+      $sql->execute();
+      $resultCheck = $sql->affected_rows;
+      if ($resultCheck > 0) {
+        header("Location: ../register.php?error=usertaken&email=".$email);
         exit();
       }
       else {
-        mysqli_stmt_bind_param($stmt, "s", $email);
-        mysqli_stmt_execute($stmt);
-        mysqli_stmt_store_result($stmt);
-        $resultCheck = mysqli_stmt_num_rows($stmt);
-        if ($resultCheck > 0) {
-          header("Location: ../register.php?error=usertaken&email=".$email);
+          $sql = $conn->prepare("INSERT INTO users (first_name, last_name, email) VALUES (?, ?, ?)");
+          $hashedPwd = password_hash($password, PASSWORD_DEFAULT);
+          $sql->bind_param("sss", $first, $last, $email);
+          $sql->execute();
+          print_r($sql->affected_rows));
+          print_r($first.' '.$last.' '.$email);
+          header("Location: ../profile.html?signup=success");
           exit();
-        }
-        else {
-          $sql = "INSERT INTO users (first_name, last_name, email) VALUES (?, ?, ?)";
-          $stmt = mysqli_stmt_init($conn);
-          if (!mysqli_stmt_prepare($stmt, $sql)) {
-            header("Location: ../register.php?error=sqlerror");
-            exit();
-          }
-          else {
-            $hashedPwd = password_hash($password, PASSWORD_DEFAULT);
-
-            mysqli_stmt_bind_param($stmt, "sss", $first, $last, $email);
-            mysqli_stmt_execute($stmt);
-            print_r(mysqli_stmt_affected_rows($stmt));
-            $resultCheck = mysqli_stmt_num_rows($stmt);
-            header("Location: ../profile.html?signup=success");
-            exit();
-          }
-        }
       }
     }
-    mysqli_stmt_close($stmt);
-    mysqli_close($conn);
+    $sql->close();
+    $conn->close();
   }
 
  ?>
