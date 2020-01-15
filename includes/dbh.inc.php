6ceb4e141d4e489e17001904ce4b2b67cc9a5366
Nata Vacheishvili <nvacheishvili@wpi.edu>
2020-01-14 23:11:01 -0500

    123123

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -2,7 +2,7 @@
 
 $dbServername = "136.243.203.211";
 $dbUsername = "root";
-$dbPassword = "pass";
+$dbPassword = "";
 $dbName = "filterfa_main_db";
 
 $conn = mysqli_connect($dbServername, $dbUsername,$dbPassword,$dbName);
