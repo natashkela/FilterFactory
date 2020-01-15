2d21a2c48c08fa9a5d6a98e1cabf949bd378d66b
Nata Vacheishvili <nvacheishvili@wpi.edu>
2020-01-14 22:32:14 -0500

    some stuff 1

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -9,18 +9,18 @@
     $confirmpass = $_POST['confirm_pass'];
 
     if(empty($first) || empty($last) || empty($email) || empty($password)) {
-      header("Location: ../register.html?error=emptyfields&uid=&email=" .$email);
+      header("Location: ../register.php?error=emptyfields&uid=&email=" .$email);
       exit();
     }
     else if ($password !== $confirmpass) {
-      header("Location: ../register.html?error=passwordcheck&mail=".$email);
+      header("Location: ../register.php?error=passwordcheck&mail=".$email);
       exit();
     }
     else {
       $sql ="SELECT email from users WHERE email=?";
       $stmt = mysqli_stmt_init($conn);
       if (!mysqli_stmt_prepare($stmt, $sql)) {
-        header("Location: ../register.html?error=sqlerror");
+        header("Location: ../register.php?error=sqlerror");
         exit();
       }
       else {
@@ -29,7 +29,7 @@
         mysqli_stmt_store_result($stmt);
         $resultCheck = mysqli_stmt_num_rows($stmt);
         if ($resultCheck > 0) {
-          header("Location: ../register.html?error=usertaken&email=".$email);
+          header("Location: ../register.php?error=usertaken&email=".$email);
           exit();
         }
         else {
@@ -37,7 +37,7 @@
             VALUES (?, ?, ?, ?, ? )";
           $stmt = mysqli_stmt_init($conn);
           if (!mysqli_stmt_prepare($stmt, $sql)) {
-            header("Location: ../register.html?error=sqlerror");
+            header("Location: ../register.php?error=sqlerror");
             exit();
           }
           else {
@@ -45,7 +45,7 @@
 
             mysqli_stmt_bind_param($stmt, "sssss", $first, $last, $email, $hashedPwd);
             mysqli_stmt_execute($stmt);
-            header("Location: ../register.html?signup=success");
+            header("Location: ../register.php?signup=success");
             exit();
           }
         }
