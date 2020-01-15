4b32fb8449d097b7f14b872a2ca35767f334b58c
Nata Vacheishvili <nvacheishvili@wpi.edu>
2020-01-14 23:15:36 -0500

    11111

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -39,18 +39,18 @@
         else {
           $sql = "INSERT INTO users (first_name, last_name, email) VALUES (?, ?, ?)";
           $stmt = mysqli_stmt_init($conn);
-//          if (!mysqli_stmt_prepare($stmt, $sql)) {
-//            header("Location: ../register.php?error=sqlerror");
-//            exit();
-//          }
-//          else {
+          if (!mysqli_stmt_prepare($stmt, $sql)) {
+            header("Location: ../register.php?error=sqlerror");
+            exit();
+          }
+          else {
             $hashedPwd = password_hash($password, PASSWORD_DEFAULT);
 
             mysqli_stmt_bind_param($stmt, "sss", $first, $last, $email);
             mysqli_stmt_execute($stmt);
             header("Location: ../profile.html?signup=success");
             exit();
-//          }
+          }
         }
       }
     }
